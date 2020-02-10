4ed9b73c41ad7e6d2123607533622b58a8e078db
Georgi Dimitrov <georgi@duglemmerdetaldrig.dk>
2020-02-10 11:21:51 +0100

    Lesson 2 - exercise 2

---
+++ lesson-2/ex-2/list-app/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Task from "./Task";
+
+class App extends React.Component {
+  todos = [
+    {
+      taskName: "Do something",
+      isDone: false
+    },
+    {
+      taskName: "Do something 2",
+      isDone: false
+    },
+    {
+      taskName: "Do something 3",
+      isDone: true
+    },
+    {
+      taskName: "Do something 4",
+      isDone: false
+    },
+    {
+      taskName: "Do something 5",
+      isDone: true
+    }
+  ];
+
+  render() {
+    let tasksArr = [];
+    this.todos.forEach((todo, index) => {
+      tasksArr.push(
+        <Task
+          key={index}
+          taskData={{ taskName: todo.taskName, isDone: todo.isDone }}
+        />
+      );
+    });
+    return <div>{tasksArr}</div>;
+  }
+}
+
+export default App;
