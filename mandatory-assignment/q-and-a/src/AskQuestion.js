4232e6fe9ce5e35aae52b6a5c4c6070f143979f9
Georgi Dimitrov <georgi@duglemmerdetaldrig.dk>
2020-02-24 11:06:39 +0100

    Creating components

---
+++ mandatory-assignment/q-and-a/src/AskQuestion.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class AskQuestion extends React.Component {
+  render() {
+    return <div className="AskQuestion"></div>;
+  }
+}
+
+export default AskQuestion;
---
+++ mandatory-assignment/q-and-a/src/Questions.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Questions extends React.Component {
+  render() {
+    return <div className="Questions"></div>;
+  }
+}
+
+export default Questions;
