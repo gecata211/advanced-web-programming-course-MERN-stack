0255a8abc532d2105682ca225ccefa5cc28c138b
Georgi Dimitrov <georgi@duglemmerdetaldrig.dk>
2020-03-23 12:58:47 +0100

    Removing other folders from branchj

--- lesson-1/ex-5/app.js
+++
@@ -1,6 +0,0 @@
-const isItFriday = require('./index');
-if (isItFriday.isItFriday(new Date('Thu Feb 06 2020 11:00:06 GMT+0100'))) {
-  console.log("Today is friday!");
-} else {
-  console.log("Today is not friday.");
-}
--- lesson-1/ex-5/index.js
+++
@@ -1,9 +0,0 @@
-const moment = require('moment');
-
-function isItFriday(date) {
-    return moment(date).format('dddd') === 'Friday' ? true : false;
-}
-
-module.exports = {
-    isItFriday: isItFriday
-};
